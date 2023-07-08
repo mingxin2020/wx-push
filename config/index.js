09f3358200070756ea141cb5bb14bd9ebff65dbd
mingxin2020 <103556383+mingxin2020@users.noreply.github.com>
2023-07-08 15:13:37 +0800

    index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx0b30cf9b0b714cc8",
+  appsecret: "119fe5c029a17082f6ebca271c86c5d7",
+  template_id: “008u0EqPH_FKpa0GZseyNKCubx6cR9jq89rBdXaliVs",
+  touser: "oPjZW6lfOsDINEk3BQ8iWhYYwhIw",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "安徽合肥",
+  first_date: "2020-01-08",
+  
 };
